29dd4d5ba02684fe6ac24a119d81ebb24f5639cc
ilj900 <ilj900@gmail.com>
2024-06-04 23:24:54 +0200

    pricing and admin added

--- gpt_telegram_bot.py
+++ gpt_telegram_bot.py
@@ -7,7 +7,9 @@ import sqlite3
 
 load_dotenv()
 statistics_db = "user_statistics.db"
-
+input_price_per_token = 0.0005
+output_price_per_token = 0.0015
+price_per_image = 0.08
 
 def initialize_database(database_name: str):
     connection = sqlite3.connect(database_name)
@@ -16,10 +18,15 @@ def initialize_database(database_name: str):
                       user_name TEXT PRIMARY KEY,
                       input_token_count INTEGER,
                       output_token_count INTEGER,
-                      images_generated INTEGER)
+                      images_generated INTEGER,
+                      current_balance FLOAT)
                    ''')
     connection.commit()
     connection.close()
+
+
+def get_debit(input_token_count, output_token_count, images_generated):
+    return input_token_count * input_price_per_token + output_token_count * output_price_per_token + images_generated * price_per_image
 
 
 def get_statistics(user_name: str, database_name: str):
@@ -28,6 +35,7 @@ def get_statistics(user_name: str, database_name: str):
     input_token_count = 0
     output_token_count = 0
     images_generated = 0
+    current_balance = 0
 
     cursor.execute('SELECT input_token_count FROM statistics WHERE user_name = ?', (user_name,))
     result = cursor.fetchone()
@@ -44,8 +52,42 @@ def get_statistics(user_name: str, database_name: str):
     if result:
         images_generated = result[0]
 
+    cursor.execute('SELECT current_balance FROM statistics WHERE user_name = ?', (user_name,))
+    result = cursor.fetchone()
+    if result:
+        current_balance = result[0]
+
     connection.close()
-    return [input_token_count, output_token_count, images_generated]
+    return [input_token_count, output_token_count, images_generated, current_balance]
+
+
+def is_active(user_name: str, database_name: str):
+    statistics = get_statistics(user_name, database_name)
+    usage = get_debit(statistics[0], statistics[1], statistics[2])
+    if usage < statistics[3]:
+        return True
+    return False
+
+
+def account_payment(user_name: str, paid, database_name: str):
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+    cursor.execute("SELECT current_balance FROM statistics WHERE user_name=?", (user_name,))
+    result = cursor.fetchone()
+    if result:
+        new_val1 = result[0] + paid
+        cursor.execute('''
+                            UPDATE statistics
+                            SET current_balance=?
+                            WHERE user_name=?
+                        ''', (new_val1, user_name))
+    else:
+        cursor.execute('''
+                            INSERT INTO statistics (user_name, input_token_count, output_token_count, images_generated, current_balance)
+                            VALUES (?, ?, ?, ?, ?)
+                        ''', (user_name, 0, 0, 0, paid))
+    connection.commit()
+    connection.close()
 
 
 def update_token_count(user_name: str, input_token_count, output_token_count, images_generated, database_name: str):
@@ -64,9 +106,9 @@ def update_token_count(user_name: str, input_token_count, output_token_count, im
                 ''', (new_val1, new_val2, new_val3, user_name))
     else:
         cursor.execute('''
-                    INSERT INTO statistics (user_name, input_token_count, output_token_count, images_generated)
-                    VALUES (?, ?, ?, ?)
-                ''', (user_name, input_token_count, output_token_count, images_generated))
+                    INSERT INTO statistics (user_name, input_token_count, output_token_count, images_generated, current_balance)
+                    VALUES (?, ?, ?, ?, ?)
+                ''', (user_name, input_token_count, output_token_count, images_generated, 0.1))
 
     connection.commit()
     connection.close()
@@ -186,9 +228,10 @@ def split_long_message(long_message: str):
 
 
 class TelegramBot:
-    def __init__(self, token, whitelist, bot_name):
+    def __init__(self, token, whitelist, admin, bot_name):
         self.whitelist = whitelist
         self.bot_name = bot_name
+        self.admin = admin
         self.MODEL_NAME = "gpt-4o"
         self.DALLE_VERSION = "dall-e-3"
         self.DALLE_RESOLUTION = "1024x1024"
@@ -200,7 +243,7 @@ class TelegramBot:
         self.application.add_handler(CommandHandler('img', self.generate_image))
         self.application.add_handler(CommandHandler('help', self.display_help))
         self.application.add_handler(CommandHandler('usage', self.get_usage_stat))
-        self.application.add_handler(CommandHandler('test', self.test))
+        self.application.add_handler(CommandHandler('pay', self.process_payment))
         self.application.add_handler(MessageHandler(filters.COMMAND, self.unknown))
         self.chat_history = []
         self.gpt_client = OpenAI()
@@ -226,24 +269,33 @@ class TelegramBot:
         image_url = response.data[0].url
         return image_url
 
-    async def test(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        if not str(update.message.from_user.username) in self.whitelist:
+
+    async def process_payment(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if not update.message.from_user.username == self.admin:
             return
 
-        test_message = str()
-        with open('test.txt') as file:
-            line = file.readline()
-            while line:
-                test_message += line
-                line = file.readline()
+        splited_data = update.message.text.split()
+        username = splited_data[1].rstrip(' ').lstrip(' ')
+        if not username in self.whitelist:
+            return
 
-        messages = split_long_message(test_message)
-        for message in messages:
-            await context.bot.send_message(chat_id=update.effective_chat.id, parse_mode='Markdown', text=message)
+        try:
+            add_to_balance = float(splited_data[2].rstrip(' ').lstrip(' '))
+        except ValueError:
+            return
+
+        account_payment(username, add_to_balance, statistics_db)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=str(add_to_balance) + "€ was added to " + username + " account.")
+
 
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not str(update.message.from_user.username) in self.whitelist:
             return
+
+        if not is_active(str(update.message.from_user.username), statistics_db):
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="You don't have enough € on you account.")
+            return
+
         response = self.ask_gpt(update)
         update_token_count(str(update.message.from_user.username), response.usage.prompt_tokens, response.usage.completion_tokens, 0, statistics_db)
         await context.bot.send_message(chat_id=update.effective_chat.id, parse_mode='Markdown', text=response.choices[0].message.content)
@@ -251,8 +303,27 @@ class TelegramBot:
     async def get_usage_stat(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not str(update.message.from_user.username) in self.whitelist:
             return
-        token_count = get_statistics(str(update.message.from_user.username), statistics_db)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Your total input, output token count and images generated are: " + str(token_count[0]) + "/" + str(token_count[1]) + "/" + str(token_count[2]) + " respectively")
+        splited_text = update.message.text.split()
+        if len(splited_text) == 1:
+            statistics = get_statistics(str(update.message.from_user.username), statistics_db)
+            debit = get_debit(statistics[0], statistics[1], statistics[2])
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=("Your total input, output token count and images generated are:\n" + str(statistics[0]) + "/" + str(statistics[1]) + "/" + str(statistics[2]) + "\n"
+                                                                                   + "Your remaining balance is " + str(statistics[3] - debit) + "€"))
+            return
+
+        if len(splited_text) == 2:
+            user_name = splited_text[1].rstrip(' ').lstrip(' ')
+            if str(update.message.from_user.username) == self.admin and user_name in self.whitelist:
+                statistics = get_statistics(user_name, statistics_db)
+                debit = get_debit(statistics[0], statistics[1], statistics[2])
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=(user_name + "'s total input, output token count and images generated are:\n" + str(statistics[0]) + "/" + str(statistics[1]) + "/" + str(statistics[2]) + "\n"
+                                                                                       + user_name + "'s current remaining is " + str(statistics[3] - debit) + "€"))
+                return
+
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="User: " + user_name + " was not found.")
+            return
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Too much parameters for the command.")
 
     async def new_brief_chat(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not str(update.message.from_user.username) in self.whitelist:
@@ -287,7 +358,8 @@ class TelegramBot:
         await context.bot.send_message(chat_id=update.effective_chat.id, parse_mode='Markdown',
                                        text="/new - Starts a new chat. You can pass the \"system\" message separated by space. Example:\n```\n/new add \"Bazinga\" at the end of every reply.```\n\n" +
                                             "/n  - Starts a new chat, but the \"system\" message predefined as \"You should be extra brief.\"\n\n" +
-                                            "/img - Ask Dall-E to generate an image with provided description. Example:\n```\n/img Draw me an iPhone but it's made of sticks and stones.```")
+                                            "/img - Ask Dall-E to generate an image with provided description. Example:\n```\n/img Draw me an iPhone but it's made of sticks and stones.```" +
+                                            "/usage show you your usage statistics.")
 
     async def unknown(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not str(update.message.from_user.username) in self.whitelist:
@@ -301,5 +373,5 @@ class TelegramBot:
 if __name__ == '__main__':
     initialize_database(statistics_db)
     bot = TelegramBot(token=os.getenv('TOKEN'), whitelist=os.getenv('ENV_TELEGRAM_WHITELIST').split(','),
-                      bot_name=os.getenv('ENV_TELEGRAM_BOT_NAME'))
+                      admin=os.getenv('ENV_TELEGRAM_ADMIN'), bot_name=os.getenv('ENV_TELEGRAM_BOT_NAME'))
     bot.run()
